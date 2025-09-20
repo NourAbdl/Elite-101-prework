588bbfa8f63d748b00aaa6945cca95c81c334386
NourAbdl <nourabdelhalim89@gmail.com>
2025-09-19 19:08:53 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Welcome to the ")
+print("Welcome to the drink chatbot! Where our goal is to keep you refreshed!")
 name = input("What is your name?")
-age = input("What is your age?");
+age = input("Hello " + name + ", how old are you?");
+print("Welcome, " + name + " how can I help you today? ");
+
+print("Here are all the things I can do for you today:");
+print("1. Placeholder");
+print("2. Placeholder");
+print("3.Placeholder");
+print("4.Exit conversation");
+optionChoice = input("What would you like to do today?")
+if int(optionChoice) == 4:
+    print("Bye, " + name + "! I hope to see you soon!")
